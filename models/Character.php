bf21b586cc4c682433ec6c2938714c7ff4c668e6
marcus <ishadaha@gmail.com>
2018-06-24 18:03:41 -0400

    adding character support

--- models/Character.php
+++ models/Character.php
@@ -1,14 +1,16 @@
 <?php
 class Character {
     public function addNew($db, $data) {
-        $characterCheck = $db->prepare("select * from characters where rsn = {$data[0]['rsn']} and active = 'Y'");
+        $RSN = str_replace(' ','_',$data[0]['rsn']);
+        
+        $characterCheck = $db->prepare("select * from characters where rsn = {$RSN} and active = 'Y'");
         $characterCheck->execute();
         
         if ($characterCheck->rowCount() <= 0) {
             $osrsAPI ="http://services.runescape.com/m=hiscore_oldschool/index_lite.ws?player=";
             $hsLink = $osrsAPI . $datap[0]['rsn'];
             
-            $characterAdd = $db->prepare("insert into characters(rsn, userID, characterType, highScoreLink, active) VALUES ('{$data[0]['rsn']}', {$data[0]['userID']}, '{$data[0]['accountType']}','{$hsLink}', 'Y'");
+            $characterAdd = $db->prepare("insert into characters(rsn, userID, characterType, highScoreLink, active) VALUES ('{$RSN}', {$data[0]['userID']}, '{$data[0]['accountType']}','{$hsLink}', 'Y'");
             if($characterAdd->execute()){     
                 return json_encode(array("status" => "success"));
         } else {
