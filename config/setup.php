42639ea4a1929bba980a2f59595b13bbad031abc
Marcus Casswell <mcasswell@dubtel.com>
2018-06-24 14:38:26 -0400

    fixing issue with config

--- config/setup.php
+++ config/setup.php
@@ -8,8 +8,8 @@ session_start();
 Flight::set('flight.views.path', 'views');
 
 if( !defined( __DIR__ ) ) define( __DIR__, dirname(__FILE__) );
-include(__DIR__.'/var/www/osrsgtconfig/db_config.php');
-require_once(__DIR__ . '/var/www/osrsgt/html/models/Users.php');
+include(__DIR__.'/../../db_config.php');
+require_once(__DIR__ . '/../models/Users.php');
 
 Flight::register('db', 'PDO', array('mysql:host=' . DBHOST . ';dbname=' . DBNAME, DBUSER, DBPASS ), function($db){
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
