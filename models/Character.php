e55c0a3bdecb41268c935d3108bb71913db8cb24
marcus <ishadaha@gmail.com>
2018-06-24 18:13:00 -0400

    adding character support

--- models/Character.php
+++ models/Character.php
@@ -7,7 +7,7 @@ class Character {
         $characterCheck->execute();
         
         if ($characterCheck->rowCount() <= 0) {
-            $osrsAPI ="http://services.runescape.com/m=hiscore_oldschool/index_lite.ws?player=";
+            $osrsAPI ="/m=hiscore_oldschool/index_lite.ws?player=";
             $hsLink = $osrsAPI . $RSN;
             echo "insert into characters(rsn, userID, characterType, highScoreLink, active) VALUES ('{$RSN}', {$data[0]['userID']}, '{$data[0]['accountType']}','{$hsLink}', 'Y'";
             $characterAdd = $db->prepare("insert into characters(rsn, userID, characterType, highScoreLink, active) VALUES ('{$RSN}', {$data[0]['userID']}, '{$data[0]['accountType']}','{$hsLink}', 'Y'");
