7c2f49463fe947c00a0bf4f75b0e6db6cec9e53c
Marcus Casswell <mcasswell@dubtel.com>
2018-06-24 15:39:40 -0400

    fixing issue with config

--- views/login.php
+++ views/login.php
@@ -50,11 +50,14 @@
                     <input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="Email Address" value="">
                   </div>
                   <div class="form-group">
+                    <input type="text" name="firstname" id="firstname" tabindex="1" class="form-control" placeholder="firstname">
+                  </div>
+                  <div class="form-group">
+                    <input type="text" name="lastname" id="lastname" tabindex="1" class="form-control" placeholder="lastname">
+                  </div>
+                  <div class="form-group">
                     <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
                   </div>
-<!--                  <div class="form-group">
-                    <input type="password" name="confirm-password" id="confirm-password" tabindex="2" class="form-control" placeholder="Confirm Password">
-                  </div>-->
                   <div class="form-group">
                     <div class="row">
                       <div class="col-sm-6 col-sm-offset-3">
