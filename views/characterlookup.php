fb6332c5b3f8ca922e35dc5ea84ebf05bb08396a
marcus <ishadaha@gmail.com>
2018-06-25 19:08:25 -0400

    adding character support

--- views/characterlookup.php
+++ views/characterlookup.php
@@ -7,8 +7,7 @@ if (!defined(__DIR__))
     define(__DIR__, dirname(__FILE__));
 require_once(__DIR__ . '/../config/setup.php');
 
-print_r($_POST);
-echo $id;
+
 ?>
 <script>
     function getAllCharInfo() {
