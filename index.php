fffbb8fc6be87f066f8bfbb07cf823a5a4642632
marcus <ishadaha@gmail.com>
2018-06-25 18:33:31 -0400

    adding character support

--- index.php
+++ index.php
@@ -105,7 +105,7 @@ $app->route('GET /dashboard', function() {
     //}
     
 });
-$app->route('GET /characterlookup', function() {
+$app->route('GET /characterlookup@id@userID', function($id, $userID) {
     Flight::render('characterlookup', array());
 
     
