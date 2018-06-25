f704ea6625a49e11739e8f557a12825b2f564a46
marcus <ishadaha@gmail.com>
2018-06-25 11:57:27 -0400

    adding character support

--- views/characterlookup.php
+++ views/characterlookup.php
@@ -8,46 +8,43 @@ if (!defined(__DIR__))
 require_once(__DIR__ . '/../config/setup.php');
 
 ?>
-<h3>Add Character to track!</h3>
-
-<form id="formApplication" class="register-form mt-lg">
-    <div class="row">
-        <div class="col-sm-6">
-            <label for="rsn">Runescape Name</label>
-            <div class="form-group">
-                <input type="text" id="rsn" name="rsn" class="form-control mb-sm" placeholder="*Runescape Name" required="required" />
-            </div>
-        </div>
-        <div class="col-sm-6"></div>
-    </div>
-    <div class="row">
-        <div class="col-sm-6">
-            <label for="last_name">Account Type</label>
-            <div class="form-group">
-                <select id="account_type">
-                    <option value="normal">normal</option>
-                    <option value="ironMan">ironMan</option>
-                    <option value="HCIM">HCIM</option>
-                    <option value="UIM">UIM</option>
-                </select>
-            </div>
-        </div>
-        <div class="col-sm-6"></div>
-        <input type="hidden" id="userID" name="userID" value="<?php echo $_SESSION['userid']; ?>" />
-    </div>    
-</form>
+<script>
+    function getAllCharInfo() {
 
 
-<!-- this is where buttons will go -->
-<div class='row'>
-    <center>
-        <div id='sendContainer' class="col-sm-6">
-            <button id="sendButton"  type="button" class="btn btn-primary" onclick='addChar();'>Submit Inquiry</button>
-            <div id="status"></div>
-        </div>
-    </center>
-</div>
-<!-- This is where iframe maps will go -->
+                var data = {method: "getAll", "data": [{}]};
+
+                var url = '/characterlookup';
+//
+                $.ajax({
+                    url: url,
+                    type: 'POST',
+                    data: JSON.stringify(data),
+                    contentType: "application/json",
+                    async: false,
+                    success: function (data) {
+                        var response = $.parseJSON(data);
+                        if (response.status == "success") {
+                            $('#sendButton').css('display', 'none');
+                            $('#status').html("<h5 class=\"alert alert-success\"> SUCCESS </h5>");
+                        } else {
+                            $('#status').html("<h5 class=\"alert alert-danger\"> ERROR </h5>");
+                        }
+                    },
+                    error: function () {
+                        alert("There Was An Error Adding Link!");
+
+                    }
+                });
+    }
+</script>
+<center><h3>Add Character to track!</h3></center>
+
+
+
+
+
+
 <script>
 
 </script>
