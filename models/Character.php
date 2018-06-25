99a0ce8d995eb22062ca0a6f7d89c99cf7021429
marcus <ishadaha@gmail.com>
2018-06-25 18:57:49 -0400

    adding character support

--- models/Character.php
+++ models/Character.php
@@ -22,4 +22,23 @@ class Character {
                return json_encode(array("status" => "fail", "message" => "rsn already exist"));
         }
     }
+    public function getAll($db, $id, $userID) {
+        try{
+            $getAll = $db->prepare("select * from characters where id = {$id}");
+            $getAll->execute();
+
+
+            if($getAll->rowCount() > 0){
+               while($row = $getAll->fetch())
+               {
+                   $character[] = array("rsn" => $row['rsn'], "link" => $row['highScoreLink'], "characterType" => $row['characterType']);
+               }
+            }
+            return json_encode(array("status" => "success", "character" => $character));
+        } catch (PDOException $e) { // The authorization query failed verification
+             return json_encode(array("status" => "failed",
+                                                            "error" => "Catch Exception: " . $e->getMessage()
+             ));
+        }
+    }
 }
