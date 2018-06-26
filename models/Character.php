f9e86fb9ab736ebc26fcdceaad2718e5a2f4f4cb
marcus <ishadaha@gmail.com>
2018-06-25 21:06:15 -0400

    adding character support

--- models/Character.php
+++ models/Character.php
@@ -44,8 +44,11 @@ class Character {
     function getStats($data) {
 		// Create an array of skills
 		$skills = array('Overall', 'Attack', 'Defence', 'Strength', 'Hitpoints', 'Ranged', 'Prayer', 'Magic', 'Cooking', 'Woodcutting', 'Fletching', 'Fishing', 'Firemaking', 'Crafting', 'Smithing', 'Mining', 'Herblore', 'Agility', 'Thieving', 'Slayer', 'Farming', 'Runecraft', 'Hunter', 'Construction', 'Summoning', 'Duel Tournament', 'Bounty Hunters', 'Bounty Hunter Rogues', 'Fist of Guthix');
-
-		$url = "https://www.tip.it/runescape/json/hiscore_user?rsn={$data['character'][0]['rsn']}&old_stats=1";
+                $dataDec = json_decode($data, true);
+                $rsn = $dataDec['character'][0]['rsn'];
+                
+                
+		$url = "https://www.tip.it/runescape/json/hiscore_user?rsn={$rsn}&old_stats=1";
 
                 $ch = curl_init($url);
                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
