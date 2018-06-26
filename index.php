bd4d356fa240fc0acf6e0b968a02bea938fe4205
marcus <ishadaha@gmail.com>
2018-06-25 20:52:36 -0400

    adding character support

--- index.php
+++ index.php
@@ -112,8 +112,8 @@ $app->route('/characterlookup/@id/@userID', function($id, $userID) {
     $allData = json_decode($json);
     $data = json_decode(json_encode($allData->character[0]), true);
     
-    $stats = $character->getStats($data['rsn']);
-    print_r($stats);
+//    $stats = $character->getStats($data['rsn']);
+//    print_r($stats);
     Flight::render('characterlookup', array('id' => $id, 'userID' => $userID, 'data' => $data));
         
     
