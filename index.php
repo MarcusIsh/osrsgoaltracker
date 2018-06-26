02f4b35da976105cc9665f79882c7050e2c64d9f
Marcus Casswell <mcasswell@dubtel.com>
2018-06-26 09:29:47 -0400

    fixing issue with config

--- index.php
+++ index.php
@@ -108,10 +108,11 @@ $app->route('GET /dashboard', function() {
 $app->route('/characterlookup/@id/@userID', function($id, $userID) {
     $db = Flight::db();
     $character = Flight::character();
-    $allData = $character->getAll($db, $id);
+    $json = $character->getAll($db, $id);
+    $allData = json_decode($json ,true);
+    $data = $allData['character'][0];
     
-    
-    $stats = $character->getStats($allData);
+    $stats = $character->getStats($json);
 //    print_r($stats);
     Flight::render('characterlookup', array('id' => $id, 'userID' => $userID, 'data' => $data));
         
