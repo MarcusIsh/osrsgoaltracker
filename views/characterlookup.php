81a0542ac8f9ae819a6fe693facc8556b2522bc3
Marcus Casswell <mcaswell2@gmail.com>
2019-05-31 03:51:15 -0400

    setting up login

--- views/characterlookup.php
+++ views/characterlookup.php
@@ -7,7 +7,7 @@ if (!defined(__DIR__))
     define(__DIR__, dirname(__FILE__));
 require_once(__DIR__ . '/../config/setup.php');
 
-print_r($stats['overall']);
+print_r($stats->Overall);
 ?>
 <script>
     function getAllCharInfo() {
