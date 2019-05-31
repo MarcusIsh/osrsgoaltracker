39ce620a48832f57d95e16e2d0ad8839691783ef
Marcus Casswell <mcaswell2@gmail.com>
2019-05-31 03:24:01 -0400

    setting up login

--- views/dashboard.php
+++ views/dashboard.php
@@ -5,7 +5,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-print_r($_SESSION);
+//print_r($_SESSION);
 ?>
 
                   <div class="col-lg-12 main-chart">
