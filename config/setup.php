35dff373eee3e9aa3c79eb223c2f3bffa2d2a928
Marcus Casswell <mcaswell2@gmail.com>
2019-05-31 01:31:17 -0400

    adding db

--- config/setup.php
+++ config/setup.php
@@ -8,11 +8,11 @@ session_start();
 Flight::set('flight.views.path', 'views');
 
 if( !defined( __DIR__ ) ) define( __DIR__, dirname(__FILE__) );
-include(__DIR__.'/../../configs/07goaltracker_db.php');
+include(__DIR__.'/../../db_config.php');
 require_once(__DIR__.'/../models/Users.php');
 require_once(__DIR__.'/../models/Character.php');
 
-Flight::register('db', 'PDO', array('mysql:host=' . $DBHOST . ';dbname=' . $DBNAME, $DBUSER, $DBPASS ), function($db){
+Flight::register('db', 'PDO', array('mysql:host=' . DBHOST . ';dbname=' . DBNAME, DBUSER, DBPASS ), function($db){
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 });
