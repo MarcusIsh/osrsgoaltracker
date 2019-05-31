79e9484bda59c6d12f3358372528aabe0a104f60
Marcus Casswell <mcaswell2@gmail.com>
2019-05-31 17:05:53 -0400

    setting up character page

--- views/login.php
+++ views/login.php
@@ -26,7 +26,7 @@
         <video src="assets/videos/webvid.mp4" autoplay="true"></video>
     </div>-->
    <div class="row">
-    <div class="col-md-6 col-md-offset-3" style='background-color:black; opacity: 25%'>
+    <div class="col-md-6 col-md-offset-3">
         <img src='../assets/img/rsicons/07gains-logo.png'></img>
     </div>
     <div class="col-md-6 col-md-offset-3">
