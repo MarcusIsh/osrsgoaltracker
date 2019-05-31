64231d9110a30ba78daf62516a691cca1d46e295
Marcus Casswell <mcaswell2@gmail.com>
2019-05-31 01:50:57 -0400

    adding db

--- config/setup.php
+++ config/setup.php
@@ -12,10 +12,10 @@ if( !defined( __DIR__ ) ) define( __DIR__, dirname(__FILE__) );
 require_once(__DIR__.'/../models/Users.php');
 require_once(__DIR__.'/../models/Character.php');
 
-//Flight::register('db', 'PDO', array('mysql:host=' . DBHOST . ';dbname=' . DBNAME, DBUSER, DBPASS ), function($db){
-//    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-//});
+Flight::register('db', 'PDO', array('mysql:host=' . DBHOST . ';dbname=' . DBNAME, DBUSER, DBPASS ), function($db){
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+});
 
 //if(ENVIRONMENT == 'development') {
 //    error_reporting(E_ALL);
