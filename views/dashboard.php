5a40da5d6e14a9479e94f25ff1602f07ac947403
Marcus Casswell <mcaswell2@gmail.com>
2019-05-31 17:50:19 -0400

    editing dashboard page

--- views/dashboard.php
+++ views/dashboard.php
@@ -7,21 +7,23 @@
  */
 //print_r($_SESSION);
 ?>
-        <div class="row">
-                            <h1>Thank you for joining O7gains.com(ALPHA)</h1> </br>
-                            07gains is an Old School RuneScape goal tracking application. Here you will be able to track multiple characters and set goals for them.</br>
-                            Be it stats, items, quest or achievements. </br>
-                            </br>
-                            Please note that this is an alpha build and somethings will not work or make sense. Some features will be here oneday and gone the next. </br>
-                            I am currently the only developer working on this project, and I am doing it for fun. If you have a good suggestion, I will try to implement it the best I can. </br>
-                            </br>
-                            Also, while this site is secured using SSL and strong password algorithms and a private database, it is best that you do NOT use your password affiliated with RuneScape. PERIOD. <br>
-                            </br>
-                            Please feel free to contact me ingame(rsn:M Y S Q L, fe Raekwon) or on twitter <a href='https://twitter.com/Ishadaha'>@ishadaha</a> and I will get back to you ASAP.</br>
-                            
-                            Thanks,</br>
-                            Ishadaha
-                        </div>
+        <div class="row" style='margin-left: 5px;'>
+            <h1>Thank you for joining O7gains.com(ALPHA)</h1> </br>
+            <span tyle='margin-left: 10px;'>
+                07gains is an Old School RuneScape goal tracking application. Here you will be able to track multiple characters and set goals for them.</br>
+                Be it stats, items, quest or achievements. </br>
+                </br>
+                Please note that this is an alpha build and somethings will not work or make sense. Some features will be here oneday and gone the next. </br>
+                I am currently the only developer working on this project, and I am doing it for fun. If you have a good suggestion, I will try to implement it the best I can. </br>
+                </br>
+                Also, while this site is secured using SSL and strong password algorithms and a private database, it is best that you do NOT use your password affiliated with RuneScape. PERIOD. <br>
+                </br>
+                Please feel free to contact me ingame(rsn:M Y S Q L, fe Raekwon) or on twitter <a href='https://twitter.com/Ishadaha'>@ishadaha</a> and I will get back to you ASAP.</br>
+
+                Thanks,</br>
+                Ishadaha
+            </span>
+        </div>
                   <div class="col-lg-12 main-chart">
                   
                   	
