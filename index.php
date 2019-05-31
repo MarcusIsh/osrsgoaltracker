488150c061c1ded5a371a01042cc875b0af9d83b
Marcus Casswell <mcaswell2@gmail.com>
2019-05-31 01:46:18 -0400

    adding db

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
 
-//use \Firebase\JWT\JWT;
+use \Firebase\JWT\JWT;
 
-//require 'vendor/autoload.php';
-//require 'config/setup.php';
-//use flight\Engine;
+require 'vendor/autoload.php';
+require 'config/setup.php';
+use flight\Engine;
 
 $app = new Engine();
 
